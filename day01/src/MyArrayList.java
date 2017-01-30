bc8f8bdf0c492d1c2dbd81c6be4c217cb059733f
Nitya Dhanushkodi <nityadhanushkodi@gmail.com>
2017-01-30 12:37:19 -0500

    day01

--- day01/src/MyArrayList.java
+++ day01/src/MyArrayList.java
@@ -13,9 +13,10 @@ public class MyArrayList {
 	}
 
 	public void add(Cow c) {
-		//if(size==elems.length){
-		//doubleSize();
-	   //}
+		//What is the condition to expand underlying array?
+		if(size==elems.length){
+			doubleElems();
+	    }
 		elems[size] = c;
 		size++;
 	}
@@ -39,11 +40,57 @@ public class MyArrayList {
 			elemToMoveUp++;
 		}
 
+		//left over elem needs to be nulled
+		elems[elemToMoveUp-1] = null;
 		size = size - 1;
+
+		//What's the condition that you want to halve the size of array?
+		if(size <= elems.length/4 && elems.length > 10 ){
+			halveElems();
+		}
+
 		return removed;
 	}
 
 	public void add(int index, Cow c) {
-		// TODO
+		//index out of bounds
+		//could add at end
+		if(index > size || index < 0) throw new IndexOutOfBoundsException("Index out of bounds");
+
+		if(size == elems.length){
+			doubleElems();
+		}
+
+		//shift up elements on right of index
+		for(int i=size-1; i>=index; i--){
+			elems[i+1] = elems[i];
+		}
+
+		elems[index] = c;
+		size = size + 1;
+	}
+
+	//probably could consolidate into 'resize' method for
+	//less repeated code
+
+	private void doubleElems(){
+		Cow[] newElems = new Cow[elems.length * 2];
+
+		for(int i=0; i<size; i++){ //*could use elems.length only if clearing
+			newElems[i] = elems[i];
+		}
+
+		this.elems = newElems; //don't need keyword this
+	}
+
+	private void halveElems(){
+		Cow[] newElems = new Cow[elems.length/2];
+
+		for(int i = 0; i<size; i++){
+			newElems[i] = elems[i];
+		}
+
+		elems = newElems;
+
 	}
 }
