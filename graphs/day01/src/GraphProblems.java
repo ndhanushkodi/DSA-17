02408e137f581c68a6f917353335932e33d64853
Nitya Dhanushkodi <nityadhanushkodi@gmail.com>
2017-03-30 12:15:02 -0400

    g

--- graphs/day01/src/GraphProblems.java
+++ graphs/day01/src/GraphProblems.java
@@ -1,20 +1,73 @@
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class GraphProblems {
 
+    private static void dfsConnected(Graph g, int v, Set<Integer> visited){
+        visited.add(v);
+
+        for (int n : g.getNeighbors(v)){
+            if(!visited.contains(n)){
+                dfsConnected(g, n, visited);
+            }
+        }
+    }
+
     public static boolean connected(Graph g, int v, int u) {
-        // TODO
-        return false;
+        Set<Integer> visited = new HashSet<>();
+        dfsConnected(g, v, visited);
+        return visited.contains(u);
     }
 
     public static List<Integer> topologicalOrder(Digraph g) {
-        // TODO
-        return null;
+        LinkedList<Integer> topoOrder = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        for (int v : g.vertices())
+            if (!visited.contains(v))
+                dfsTopo(g, v, topoOrder, visited);
+        return topoOrder;
+    }
+
+    private static void dfsTopo(Digraph g, int v, LinkedList<Integer> topoOrder, Set<Integer> visited) {
+        visited.add(v);
+        for (int n : g.getNeighbors(v))
+            if (!visited.contains(n))
+                dfsTopo(g, n, topoOrder, visited);
+        // Add the current vertex to the head of a LinkedList
+        topoOrder.addFirst(v);
     }
 
     public static boolean hasCycle(UndirectedGraph g) {
-        // TODO
+        Set<Integer> marked = new HashSet<>();
+        // Check if there's a cycle starting from each vertex using DFS
+        for (int v : g.vertices()) {
+            if (!marked.contains(v)) {
+                if (dfsCycle(g, v, -1, marked)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
+    private static boolean dfsCycle(Graph g, int v, int cameFrom, Set<Integer> marked) {
+        marked.add(v);
+        for (int n : g.getNeighbors(v)) {
+            if (!marked.contains(n)) {
+                if (dfsCycle(g, n, v, marked)) {
+                    return true;
+                }
+            }
+            // if we reach a marked vertex and it's not the one we came from, we've found
+            // a cycle.
+            else if (n != cameFrom) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
--- graphs/day01/src/ReviewProblem.java
+++ graphs/day01/src/ReviewProblem.java
@@ -1,8 +1,63 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ReviewProblem {
 
-    public static int minimumLengthSubArray(int[] A, int desiredSum) {
-        // TODO
-        return 0;
+    public static int minimumLengthSubArray2(int[] A, int desiredSum) {
+        int start = 0, end = 0, sum = 0, minLen = Integer.MAX_VALUE;
+        while (end < A.length) {
+            while (end < A.length && sum < desiredSum) {
+                sum += A[end];
+                end++;
+            }
+            if (sum < desiredSum) break;
+            while (start < end && sum >= desiredSum){
+                sum -= A[start];
+                start++;
+            }
+            if (end - start + 1 < minLen) {
+                minLen = end - start + 1;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+
+    public static int minimumLengthSubArrayArr(int[] A, int desiredSum){
+        if (A == null || A.length == 0)
+            return 0;
+        int i = 0, j = 0, sum = 0, min = Integer.MAX_VALUE;
+        while (j < A.length) {
+            sum += A[j];
+            j = j+1;
+            while (sum >= desiredSum) {
+                min = Math.min(min, j - i);
+                sum -= A[i];
+                i = i+1;
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+
+    public static int minimumLengthSubArray(int[] A, int desiredSum){
+        if (A == null || A.length == 0)
+            return 0;
+        Queue<Integer> q = new  LinkedList<>();
+        int index = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        while (index < A.length) {
+            while(sum < desiredSum) {
+                q.add(A[index]);
+                sum += A[index];
+                index = index + 1;
+            }
+            while (sum >= desiredSum) {
+                min = Math.min(min, q.size());
+                sum -= q.poll();
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
     }
 
 }
